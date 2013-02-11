da70d82c73cba1b8a342fe3a98be179dd2e631d3
Gustavo Luiz Duarte <gustavold@gmail.com>
2013-02-10 23:22:55 -0200

    Fix vim comment line

--- netsfs.c
+++ netsfs.c
@@ -1,4 +1,5 @@
-/* vim: tabstop=4:softtabstop=4:shiftwidth=4:expandtab */
+/* vim: tabstop=4:softtabstop=4:shiftwidth=4:expandtab 
+ * */
 #include <linux/fs.h>
 #include <linux/module.h>
 #include <linux/pagemap.h>
